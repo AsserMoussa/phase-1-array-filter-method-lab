3f019f4d364cfbe4521cbac3a954cc16c0384cb8
assermoussa <asser.y.moussa@gmail.com>
2024-01-29 23:30:22 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+
+
+  function findMatching(mydrivers, s){
+
+const result = mydrivers.filter((driver)=>driver.toLowerCase()===s.toLowerCase())
+return result;
+   }
+
+   function fuzzyMatch(mydrivers, s){
+    const result = mydrivers.filter((driver)=>driver.startsWith(s))
+return result;
+   }
+
+   function matchName(mydrivers, s){
+    const result = mydrivers.filter((driver)=>driver.name === s)
+return result;
+   }
